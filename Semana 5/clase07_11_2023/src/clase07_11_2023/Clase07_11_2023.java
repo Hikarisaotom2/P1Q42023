2d0476bded6e001c62ea38ce8fd4a15024f5f95e
Claudia Cortes <clau_cortes@unitec.edu>
2023-11-08 08:27:55 -0600

    Clase 8 de noviembre, metodos con return

--- Semana 5/clase07_11_2023/src/clase07_11_2023/Clase07_11_2023.java
+++ Semana 5/clase07_11_2023/src/clase07_11_2023/Clase07_11_2023.java
@@ -1,5 +1,7 @@
 package clase07_11_2023;
 
+import java.util.Scanner;
+
 /**
  * @author Clau
  */
@@ -9,17 +11,47 @@ public class Clase07_11_2023 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Llamados 
-        //Argumentos
-        PascalCase("Hola", 5);
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese una cadena: ");
+        String mensaje = entrada.nextLine();
+
+        PascalCase(mensaje, 1);
         String miCadena = "hola mundo";
-        PascalCase(miCadena, 9);
-        // String cadenaScanner = entrada.nextLine();
-        //PascalCase(cadenaScanner,7);
-        EjercicioPalabras("Camiseta");
-        EjercicioPalabras("Camisetas");
+        PascalCase(miCadena, 1);
+        String resp = EjercicioPalabras("hola");
+        String resp2 = EjercicioPalabras("tre");
+        System.out.println("La respuesta es " + resp);
+        System.out.println("La respuesta es " + resp2);
+        ejemplo(mensaje);
+        int numero = leerNumeroValidado();
+        int numero2 = leerNumeroValidado();
+        int suma=Calculadora(numero,numero2,"+");
+        int resta=Calculadora(numero,numero2,"-");
+        int multi=Calculadora(numero,numero2,"*");
+        int div=Calculadora(numero,numero2,"/");
+        System.out.println("Suma "+suma);
+        System.out.println("Resta "+resta);
+        System.out.println("multi "+multi);
+        System.out.println("Divi "+div);
+        
+        
+       
+    }
+    
+    public static int leerNumeroValidado(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese un número");
+        int numero = entrada.nextInt();
+        while (numero<=0) {
+            System.out.println("Ingrese un número mayor a 0 ");
+            numero = entrada.nextInt();
+        }
+        return numero;
     }
 
+    public static void ejemplo(String mensaje) {
+        System.out.println(mensaje);
+    }
     //parametros
     public static void PascalCase(String cadena, int numero) {
         for (int i = 0; i < numero; i++) {
@@ -28,6 +60,7 @@ public class Clase07_11_2023 {
     }
 
     public static String EjercicioPalabras(String cadena) {
+        System.out.println("Entro al metodo");
         if (cadena.length() % 2 == 0) {//par 
             int mitad = cadena.length() / 2;
             String nuevaCadena = "";
@@ -49,4 +82,24 @@ public class Clase07_11_2023 {
         }
     }
 
+    public static int Calculadora(int numero1, int numero2, String operacion) {
+        int resultado = 0;
+        switch (operacion) {
+            case "+":
+                resultado = numero1 + numero2;
+                break;
+            case "-":
+                resultado = numero1 - numero2;
+                break;
+            case "*":
+                resultado = numero1 * numero2;
+                break;
+            case "/":
+                resultado = numero1 / numero2;
+                break;
+            default:
+                System.out.println("Operacion no valida");
+        }
+        return resultado;
+    }
 }
