143fe04cdb66aba730214e6a58442544c02e1866
Claudia Cortes <clau_cortes@unitec.edu>
2023-11-14 08:32:57 -0600

    Clase 13-14 noviembre: arreglos

--- Semana 5/clase09_11_2023/src/clase09_11_2023/Clase09_11_2023.java
+++ Semana 5/clase09_11_2023/src/clase09_11_2023/Clase09_11_2023.java
@@ -4,6 +4,8 @@
  */
 package clase09_11_2023;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Clau
@@ -14,7 +16,36 @@ public class Clase09_11_2023 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        /*IMPORTANTE:
+        1)Tamaño fijo:una vez determinado el tamaño, este no puede cambiar
+        2) Contenido:Los arreglos solo pueden contener un mismo tipo de dato
+        */
+        
+        // tipo_dato nombre_variable = valor_init;
+       // int [] miLista = new int[/*tamano (debe ser un entero)*/];
+//        int [] miLista = new int[5];
+        int [] miLista ={4,8,9,5};
+         miLista = new int[2];
+//        int tamano= 5;
+//        int [] miLista2 = new int[tamano];
+//        int lectura = entrada.nextInt();
+//        int [] miLista3 = new int[lectura];
+//        int valor = num();
+//        int [] miLista4 = new int[lectura];
+//        int [] miLista5 = new int[num()];
+        
+//        System.out.println("El arreglo es "+miLista4);
+//            miLista[0]=5;
+//            miLista[2]=7;
+        for (int i = 0; i < miLista.length; i++) {
+            System.out.println("Arreglo "+i+" "+miLista[i]);
+        }
+
+    }
+    
+    public static int num(){
+        return 6+3;
     }
     
 }
---
+++ Semana 6/clase13_11_2023/src/clase13_11_2023/Clase13_11_2023.java
@@ -0,0 +1,100 @@
+package clase13_11_2023;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Clau
+ */
+public class Clase13_11_2023 {
+
+    public static void main(String[] args) {
+
+        //System.out.println("De numero a ascii");
+       // String cadena = "hola,adiosMmundo,gato,perro,miau";
+        //String [] arregloSplit=cadena.split(",");
+      //  int codigoAscii= cadena.charAt(0);
+        //  int codigoAscii= 'A';
+       // System.out.println("El codigo ascii "+codigoAscii);
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese una lista de numeros separadas por , ");
+        String cadena =  entrada.next();
+        String [] valores = cadena.split(",");
+        System.out.println("el tamano era "+valores.length);
+        for (int i = 0; i < valores.length; i++) {
+            System.out.println(valores[i]);
+        }
+        for (int i = 0; i < valores.length; i++) {
+            // Valores contiene strings 
+            //NO HAY MANERA DIRECTA DE CONVERTIR DE STRING A ASCII
+            //solo podemos convertir de enteros y caracteres a ascii
+            
+            // convertir de string => numero
+            // convertir de numero => ascii/char
+            //Lllegar al codigo ascii
+        }
+        int tamano = leerNumeroValidado();
+        System.out.println("Se crea el arreglo....");
+        int[] arreglo = crearArreglo(tamano);
+        System.out.println("Agregando valores al arreglo....");
+        arreglo = llenarArreglo(arreglo);
+        System.out.println("Imprimiendo el arreglo....");
+        imprimirArreglo(arreglo);
+        System.out.println("Actualizando el arreglo....");
+        int tamanoArreglo = arreglo.length;
+        int pos = leerPosArreglo(tamanoArreglo);
+        int valorAModificar = leerNumeroValidado();
+        arreglo = ActualizarPos(pos, valorAModificar, arreglo);
+        System.out.println("Imprimiendo el arreglo actualizado....");
+        imprimirArreglo(arreglo);
+        
+    }
+
+    //0.... tamano-1
+    public static int leerPosArreglo(int tamanoArreglo) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese una pos del arreglo:");
+        int tamano = entrada.nextInt();
+        //0.... ult (tamanoArreglo-1)
+        while (tamano < 0 || tamano >= tamanoArreglo) {
+            System.out.println("Ingrese una pos del arreglo:");
+            tamano = entrada.nextInt();
+        }
+        return tamano;
+    }
+
+    public static int leerNumeroValidado() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese un número ");
+        int tamano = entrada.nextInt();
+        while (tamano <= 0) {
+            System.out.println("Ingrese un número valido ");
+            tamano = entrada.nextInt();
+        }
+        return tamano;
+    }
+
+    public static int[] crearArreglo(int tamano) {
+        int[] arreglo = new int[tamano];
+        return arreglo;
+    }
+
+    public static int[] llenarArreglo(int[] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo[i] = leerNumeroValidado();
+        }
+        return arreglo;
+    }
+
+    public static void imprimirArreglo(int[] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("[" + i + "] " + arreglo[i]);
+        }
+    }
+
+    public static int[] ActualizarPos(int pos, int valorActualizar, int[] arreglo) {
+        arreglo[pos] = valorActualizar;
+        return arreglo;
+    }
+
+}
