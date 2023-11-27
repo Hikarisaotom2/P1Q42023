b95159a759c4c07ca5d482f40f0930ca83476506
Claudia Cortes <clau_cortes@unitec.edu>
2023-11-27 08:23:18 -0600

    Clase 27 de noviembre: clases

---
+++ Semana 8/clase27_11_2023_clases/src/clase27_11_2023_clases/Clase27_11_2023_clases.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase27_11_2023_clases;
+
+/**
+ *
+ * @author Clau
+ */
+public class Clase27_11_2023_clases {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Semana 8/clase27_11_2023_clases/src/clase27_11_2023_clases/Persona.java
@@ -0,0 +1,19 @@
+
+package clase27_11_2023_clases;
+
+/**
+ *
+ * @author Clau
+ */
+public class Persona {
+    /*Atributos*/
+    String nombre;
+    int edad;
+    String Sexo;
+    String colorOjos;
+    
+    /*Metodos*/
+    public static String hablar(){
+        return "";
+    }
+}
