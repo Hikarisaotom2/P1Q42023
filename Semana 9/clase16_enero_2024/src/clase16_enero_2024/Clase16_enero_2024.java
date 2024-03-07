2384f9a63fa910cc599736f765e3463df2d7a868
Claudia Cortes <clau_cortes@unitec.edu>
2024-03-06 20:02:35 -0600

    Clase 06 de Marzo: Objetos, setters y getters

---
+++ Semana 9/clase16_enero_2024/src/clase16_enero_2024/Clase16_enero_2024.java
@@ -0,0 +1,38 @@
+package clase16_enero_2024;
+
+/**
+ *
+ * @author Clau
+ */
+public class Clase16_enero_2024 {
+    public static void main(String[] args) {
+    /*
+    Las declaración de variables en java sigue el siguiente patron: 
+    tipoDeDato nombreVariable;
+    tipoDeDato nombreVariable = valorInicial;
+*/
+    //promedio de calificaciones 
+   // promedioDeCalificaciones camelCase
+   // promedio_de_calificaciones snake_case
+   // PromedioDeCalificaciones PascalCase
+    
+    int edadPersona = 5;
+    double promedioNotas = 5.5;
+    float valoresDeci= 0;
+    long numeroLong = 9;
+    short numeroShort = 9;
+    byte numeroByte = 5;
+    char inicial = 'c';
+    boolean valorBool=true;
+    
+    //Consola
+     System.out.println("Hola mundo salto de linea");
+    System.out.print("Hola Mundo!!!!");
+   
+    
+    
+    
+    
+    }
+    
+}
